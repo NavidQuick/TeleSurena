ab9b26072f0a9aacc874ee43d3dcc9c0e98e7987
NavidQuick <navid.kakavand2@gmail.com>
2016-09-28 20:21:32 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,3 +1,5 @@
+--This Plugin Writed By @ImanDaneshi
+--Full Edit By @Navid_Quick
 local function set_bot_photo(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
@@ -6,18 +8,53 @@ local function set_bot_photo(msg, success, result)
     os.rename(result, file)
     print('File moved to:', file)
     set_profile_photo(file, ok_cb, false)
-    send_large_msg(receiver, 'Photo changed!', ok_cb, false)
+    send_large_msg(receiver, 'عکس ربات عوض شد!', ok_cb, false)
     redis:del("bot:photo")
   else
     print('Error downloading: '..msg.id)
-    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
+    send_large_msg(receiver, 'لطفا دوباره سعی کنید!', ok_cb, false)
   end
 end
+
+--فانکشن افزودن تاریخچه
+local function logadd(msg)
+	local data = load_data(_config.moderation.data)
+	local receiver = get_receiver(msg)
+	local GBan_log = 'GBan_log'
+   	if not data[tostring(GBan_log)] then
+		data[tostring(GBan_log)] = {}
+		save_data(_config.moderation.data, data)
+	end
+	data[tostring(GBan_log)][tostring(msg.to.id)] = msg.to.peer_id
+	save_data(_config.moderation.data, data)
+	local text = 'تاریخچه گروه تنظیم شد!'
+	reply_msg(msg.id,text,ok_cb,false)
+	return
+end
+
+--فانکشن حذف تاریخچه
+local function logrem(msg)
+	local data = load_data(_config.moderation.data)
+    local receiver = get_receiver(msg)
+	local GBan_log = 'GBan_log'
+	if not data[tostring(GBan_log)] then
+		data[tostring(GBan_log)] = nil
+		save_data(_config.moderation.data, data)
+	end
+	data[tostring(GBan_log)][tostring(msg.to.id)] = nil
+	save_data(_config.moderation.data, data)
+	local text = 'تاریخچه گروه حذف شد!'
+	reply_msg(msg.id,text,ok_cb,false)
+	return
+end
+
+
 local function parsed_url(link)
   local parsed_link = URL.parse(link)
   local parsed_path = URL.parse_path(parsed_link.path)
   return parsed_path[2]
 end
+
 local function get_contact_list_callback (cb_extra, success, result)
   local text = " "
   for k,v in pairs(result) do
@@ -29,40 +66,18 @@ local function get_contact_list_callback (cb_extra, success, result)
   file:write(text)
   file:flush()
   file:close()
-  send_document("user#id"..cb_extra.target,"contact_list.txt", ok_cb, false)--.txt format
-  local file = io.open("contact_list.json", "w")
-  file:write(json:encode_pretty(result))
-  file:flush()
-  file:close()
-  send_document("user#id"..cb_extra.target,"contact_list.json", ok_cb, false)--json format
+  send_document("user#id"..cb_extra.target,"contact_list.txt", ok_cb, false)--.txt فرمت
 end
-local function user_info_callback(cb_extra, success, result)
-  result.access_hash = nil
-  result.flags = nil
-  result.phone = nil
-  if result.username then
-    result.username = '@'..result.username
-  end
-  result.print_name = result.print_name:gsub("_","")
-  local text = serpent.block(result, {comment=false})
-  text = text:gsub("[{}]", "")
-  text = text:gsub('"', "")
-  text = text:gsub(",","")
-  if cb_extra.msg.to.type == "chat" then
-    send_large_msg("chat#id"..cb_extra.msg.to.id, text)
-  else
-    send_large_msg("user#id"..cb_extra.msg.to.id, text)
-  end
-end
+
 local function get_dialog_list_callback(cb_extra, success, result)
   local text = ""
-  for k,v in pairs(result) do
+  for k,v in pairsByKeys(result) do
     if v.peer then
       if v.peer.type == "chat" then
-        text = text.."group{"..v.peer.title.."}["..v.peer.id.."]("..v.peer.members_num..")"
+        text = text.."گروه{"..v.peer.title.."}["..v.peer.id.."]("..v.peer.members_num..")"
       else
         if v.peer.print_name and v.peer.id then
-          text = text.."user{"..v.peer.print_name.."}["..v.peer.id.."]"
+          text = text.."کاربر{"..v.peer.print_name.."}["..v.peer.id.."]"
         end
         if v.peer.username then
           text = text.."("..v.peer.username..")"
@@ -73,16 +88,16 @@ local function get_dialog_list_callback(cb_extra, success, result)
       end
     end
     if v.message then
-      text = text..'\nlast msg >\nmsg id = '..v.message.id
+      text = text..'\nآخرین پیام :\nآیدی گروه : '..v.message.id
       if v.message.text then
-        text = text .. "\n text = "..v.message.text
+        text = text .. "\n متن پیام : "..v.message.text
       end
       if v.message.action then
         text = text.."\n"..serpent.block(v.message.action, {comment=false})
       end
       if v.message.from then
         if v.message.from.print_name then
-          text = text.."\n From > \n"..string.gsub(v.message.from.print_name, "_"," ").."["..v.message.from.id.."]"
+          text = text.."\n از : \n"..string.gsub(v.message.from.print_name, "_"," ").."["..v.message.from.id.."]"
         end
         if v.message.from.username then
           text = text.."( "..v.message.from.username.." )"
@@ -98,19 +113,39 @@ local function get_dialog_list_callback(cb_extra, success, result)
   file:write(text)
   file:flush()
   file:close()
-  send_document("user#id"..cb_extra.target,"dialog_list.txt", ok_cb, false)--.txt format
-  local file = io.open("dialog_list.json", "w")
-  file:write(json:encode_pretty(result))
-  file:flush()
-  file:close()
-  send_document("user#id"..cb_extra.target,"dialog_list.json", ok_cb, false)--json format
+  send_document("user#id"..cb_extra.target,"dialog_list.txt", ok_cb, false)--.txt فرمت
 end
+
+local function plugin_enabled( name )
+  for k,v in pairs(_config.enabled_plugins) do
+    if name == v then
+      return k
+    end
+  end
+  return false
+end
+
+local function plugin_exists( name )
+  for k,v in pairs(plugins_names()) do
+    if name..'.lua' == v then
+      return true
+    end
+  end
+  return false
+end
+
+local function reload_plugins( )
+	plugins = {}
+  return load_plugins()
+end
+
 local function run(msg,matches)
-    local data = load_data(_config.moderation.data)
     local receiver = get_receiver(msg)
     local group = msg.to.id
-    if not is_admin(msg) then
-    	return
+	local print_name = user_print_name(msg.from):gsub("‮", "")
+	local name_log = print_name:gsub("_", " ")
+    if not is_admin1(msg) then
+    	return 
     end
     if msg.media then
       	if msg.media.type == 'photo' and redis:get("bot:photo") then
@@ -119,91 +154,144 @@ local function run(msg,matches)
       		end
       	end
     end
-    if matches[1] == "setbotphoto" then
+    if matches[1] == "تنظیم عکس ربات" then
     	redis:set("bot:photo", "waiting")
-    	return 'Please send me bot photo now'
+    	return 'لطفا عکس ربات را ارسال کنید'
     end
-    if matches[1] == "markread" then
-    	if matches[2] == "on" then
+    if matches[1] == "خواندن" then
+    	if matches[2] == "روشن" then
     		redis:set("bot:markread", "on")
-    		return "Mark read > on"
+    		return "خواندن روشن شد."
     	end
-    	if matches[2] == "off" then
+    	if matches[2] == "خاموش" then
     		redis:del("bot:markread")
-    		return "Mark read > off"
+    		return "خواندن خاموش شد."
     	end
     	return
     end
-    if matches[1] == "pm" then
-    	send_large_msg("user#id"..matches[2],matches[3])
-    	return "Msg sent"
+    if matches[1] == "ارسال" then
+    	local text = "شما یک پیام دریافت کردید از : "..(msg.from.username or msg.from.last_name).."\n\nپیام ارسالی : "..matches[3]
+    	send_large_msg("user#id"..matches[2],text)
+    	return "پیام شما ارسال شد"
     end
-    if matches[1] == "block" then
+    
+    if matches[1] == "بلاک" then
     	if is_admin2(matches[2]) then
-    		return "You can't block admins"
+    		return "شما نمی توانید ادمین ها را بلاک کنید"
     	end
     	block_user("user#id"..matches[2],ok_cb,false)
-    	return "User blocked"
+    	return "کاربر مورد نظر بلاک شد"
     end
-    if matches[1] == "unblock" then
+    if matches[1] == "آنبلاک" then
     	unblock_user("user#id"..matches[2],ok_cb,false)
-    	return "User unblocked"
+    	return "کاربر آنبلاک شد"
     end
-    if matches[1] == "import" then--join by group link
+    if matches[1] == "ورود به" then--ورود با لینک
     	local hash = parsed_url(matches[2])
     	import_chat_link(hash,ok_cb,false)
     end
-    if matches[1] == "contactlist" then
-      get_contact_list(get_contact_list_callback, {target = msg.from.id})
-      return "I've sent contact list with both json and text format to your private"
-    end
-    if matches[1] == "addcontact" and matches[2] then    add_contact(matches[2],matches[3],matches[4],ok_cb,false)
-      return "Number "..matches[2].." add from contact list"
-    end
-    if matches[1] == "delcontact" then
-      del_contact("user#id"..matches[2],ok_cb,false)
-      return "User "..matches[2].." removed from contact list"
-    end
-    if matches[1] == "dialoglist" then
-      get_dialog_list(get_dialog_list_callback, {target = msg.from.id})
-      return "I've sent dialog list with both json and text format to your private"
-    end
-    if matches[1] == "whois" then
-      user_info("user#id"..matches[2],user_info_callback,{msg=msg})
-    end
-    if matches[1] == "sync_gbans" then
-    	if not is_sudo(msg) then-- Sudo only
+    if matches[1] == "لیست مخاطبین" then
+	    if not is_sudo(msg) then-- فقط سودو
     		return
     	end
-    	local url = "http://seedteam.org/Teleseed/Global_bans.json"
-    	local SEED_gbans = http.request(url)
-    	local jdat = json:decode(SEED_gbans)
-    	for k,v in pairs(jdat) do
-  		redis:hset('user:'..v, 'print_name', k)
-  		banall_user(v)
-      		print(k, v.." Globally banned")
-    	end
+      get_contact_list(get_contact_list_callback, {target = msg.from.id})
+      return "لیست مخاطبین به پیوی شما ارسال شد"
     end
+    if matches[1] == "حذف مخاطب" then
+	    if not is_sudo(msg) then-- فقط سودو
+    		return
+    	end
+      del_contact("user#id"..matches[2],ok_cb,false)
+      return "کاربر "..matches[2].." از لیست مخاطبین حذف شد"
+    end
+    if matches[1] == "افزودن مخاطب" and is_sudo(msg) then
+    phone = matches[2]
+    first_name = matches[3]
+    last_name = matches[4]
+    add_contact(phone, first_name, last_name, ok_cb, false)
+   return "کاربری با شماره +"..matches[2].." افزوده شد"
+end
+ if matches[1] == "ارسال مخاطب" and is_sudo(msg) then
+    phone = matches[2]
+    first_name = matches[3]
+    last_name = matches[4]
+    send_contact(get_receiver(msg), phone, first_name, last_name, ok_cb, false)
+end
+ if matches[1] == "مخاطب من" and is_sudo(msg) then
+	if not msg.from.phone then
+		return "شرمنده من شماره شما را ندارم"
+    end
+    phone = msg.from.phone
+    first_name = (msg.from.first_name or msg.from.phone)
+    last_name = (msg.from.last_name or msg.from.id)
+    send_contact(get_receiver(msg), phone, first_name, last_name, ok_cb, false)
+end
+
+    if matches[1] == "لیست گفتگو" then
+      get_dialog_list(get_dialog_list_callback, {target = msg.from.id})
+      return "من لیست گفتگو را به پیوی شمار ارسال کردم"
+    end
+    if matches[1] == "درباره" then
+      user_info("user#id"..matches[2],user_info_callback,{msg=msg})
+    end
+	if matches[1] == 'آپدیت آیدی' then
+		local data = load_data(_config.moderation.data)
+		local long_id = data[tostring(msg.to.id)]['long_id']
+		if not long_id then
+			data[tostring(msg.to.id)]['long_id'] = msg.to.peer_id 
+			save_data(_config.moderation.data, data)
+			return "آیدی آپدیت شد"
+		end
+	end
+	if matches[1] == 'افزودن تاریخچه' and not matches[2] then
+		if is_log_group(msg) then
+			return "تاریخچه ای از قبل وجود دارد."
+		end
+		print("تاریخچه گروه "..msg.to.title.."("..msg.to.id..") افزوده شد.")
+		savelog(msg.to.id, name_log.." ["..msg.from.id.."] تاریخچه گروه را افزود.")
+		logadd(msg)
+	end
+	if matches[1] == 'حذف تاریخچه' and not matches[2] then
+		if not is_log_group(msg) then
+			return "تاریخچه ای وجود ندارد."
+		end
+		print("تاریخچه گروه "..msg.to.title.."("..msg.to.id..") حذف شد")
+		savelog(msg.to.id, name_log.." ["..msg.from.id.."] تاریچه را حذف کرد.")
+		logrem(msg)
+	end
     return
 end
+
+local function pre_process(msg)
+  if not msg.text and msg.media then
+    msg.text = '['..msg.media.type..']'
+  end
+  return msg
+end
+
 return {
   patterns = {
-	"^[!/](pm) (%d+) (.*)$",
-	"^[!/](import) (.*)$",
-	"^[!/](unblock) (%d+)$",
-	"^[!/](block) (%d+)$",
-	"^[!/](markread) (on)$",
-	"^[!/](markread) (off)$",
-	"^[!/](setbotphoto)$",
+	"^(ارسال) (%d+) (.*)$",
+	"^(ورود به) (.*)$",
+	"^(انبلاک) (%d+)$",
+	"^(بلاک) (%d+)$",
+	"^(خواندن) (روشن)$",
+	"^(خواندن) (خاموش)$",
+	"^(تنظیم عکس ربات)$",
+	"^(لیست مخاطبین)$",
+	"^(لیست گفتگو)$",
+	"^(حذف مخاطب) (%d+)$",
+	"^(افزودن مخاطب) (.*) (.*) (.*)$", 
+	"^(ارسال مخاطب) (.*) (.*) (.*)$",
+	"^(مخاطب من)$",
+	"^(راه اندازی مجدد)$",
+	"^(آپدیت آیدی)$",
+	"^(افزودن تاریخچه)$",
+	"^(حذف تاریخچه)$",
 	"%[(photo)%]",
-	"^[!/](contactlist)$",
-	"^[!/](dialoglist)$",
-	"^[!/](delcontact) (%d+)$",
-        "^[!/](addcontact) (.*) (.*) (.*)$",
-	"^[!/](whois) (%d+)$",
-	"^/(sync_gbans)$"--sync your global bans with seed
   },
   run = run,
+  pre_process = pre_process
 }
---By @imandaneshi :)
---https://github.com/SEEDTEAM/TeleSeed/blob/master/plugins/admin.lua
+--This Plugin Writed By @ImanDaneshi
+--Full Edit By @Navid_Quick
